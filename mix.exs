0537f65dc9f31dcb7c3e9e9978b9bf2f75df9fd8
Daniel Spofford <daniel.spofford@verypossible.com>
2018-05-27 10:12:52 -0500

    Add level option
    Why
    ---
    
    * So that prompts can be printed in colors other than white (info)
    
    How
    ---
    
    * Add puts/2
    * ask/3, gets/1, and puts/2 support `:level` option

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Clhi.MixProject do
   def project do
     [
       app: :clhi,
-      version: "0.1.0",
+      version: "0.2.0",
       elixir: "~> 1.6",
       start_permanent: Mix.env() == :prod,
       deps: [{:ex_doc, "~> 0.0", only: :dev}],
